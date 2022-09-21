e30d745b5de9cb327d275f3072a2a57653870b86
yvescabier <yscabier@gmail.com>
2022-09-21 22:11:57 +0200

    controle securité

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,6 @@
-const express = require("express"); 
+const express = require("express");
+
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -12,11 +14,17 @@ const userLikes = require("./controllers/like");
 
 require("dotenv").config();
 
+const helmet = require("helmet")
+
+// utilisation du module 'helmet' pour la sécurité en protégeant l'application de certaines vulnérabilités
+// il sécurise nos requêtes HTTP, sécurise les en-têtes, contrôle la prélecture DNS du navigateur, empêche le détournement de clics
+// et ajoute une protection XSS mineure et protège contre le reniflement de TYPE MIME
+// cross-site scripting, sniffing et clickjacking
+
 const rateLimit = require("express-rate-limit");
 
 app.use(express.json()); // cd middleware intercepte toutes les requetes qui ont un content type json qui contiennent du json et nous mettent à disposition ce contenu sur l'objet requete dans req.body
-
-
+//const helmet = require("helmet");
 mongoose
   .connect(
     "mongodb+srv://" +
@@ -24,16 +32,17 @@ mongoose
       "@cluster0.3me1g.mongodb.net/" +
       process.env.DB_NAME +
       "?retryWrites=true&w=majority",
-      { useNewUrlParser: true, useUnifiedTopology: true }
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 // (empèche les requette au image statique (mais seulement depuis le front)) REGLE DES FAILLES DE SECURITE DANS LES ENTETE HTTP
-// app.use(helmet());
+
 app.use((req, res, next) => {
   // 1er middleware qui sera executé par le server et il est général on va pas y spécifier de route (ex api/stuff)
-  res.setHeader("Access-Control-Allow-Origin", "*"); // l'origine qui à le droit d'accéder à notre api c'est tous le monde donc l'étoile *)
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8081"); //L'entête Access-Control-Allow-Origin renvoie une réponse indiquant si les ressources peuvent être partagées avec une origine donnée
+
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
@@ -42,8 +51,10 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
   ); //ainsi que certaines requêtes
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
+app.use(bodyParser.json());
 // RATE LIMIT PERMET DE LIMITER LE NOMBRE DE REQUETE QUI PEUVENT ETRE FAITE SUR LAPI SUR UNE PERIODE DONNER PAR EX ON LIMITE 100 REQUETES PAR 15 MINUTES
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
@@ -55,11 +66,11 @@ const limiter = rateLimit({
 // Apply the rate limiting middleware to all requests
 app.use(limiter);
 /** la route /images renvoie les fichier brute (statique) du répertoire images*/
-app.use('/images', express.static('images'));
-
-app.use('/api/sauces', saucesRoutes);
-app.use('/api/auth', usersRoutes);
-app.use('/api/sauces', userLikes);
+app.use("/images", express.static("images"));
+app.use(helmet({crossOriginEmbedderPolicy: false}))
+app.use("/api/sauces", saucesRoutes);
+app.use("/api/auth", usersRoutes);
+app.use("/api/sauces", userLikes);
 //app.use("/api/sauces", userDislikes);
 
 module.exports = app;
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
-
+// cd fichier rassemble les routes sensible de notre api
+//on construit le middleware qui va vérifié le token reçu depuis le frontend et permettre uniquement  des requ^tes authentifié de réussir
 module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(' ')[1];//ça va nous renvoyer le tableau avec bearer et token et prendre le 2eme element du tableau
-        const decodedToken = jwt.verify(token,'RANDOM_TOKEN_SECRET')//débloquer le token
-        const userId = decodedToken.userId;//on recupere le user id 
-        req.auth = {userId};
+        const decodedToken = jwt.verify(token,'RANDOM_TOKEN_SECRET')//on décode le token avec vérify 
+        const userId = decodedToken.userId;//on recupere la propriété du  token avec son user id 
+        req.auth = {userId};//et on rajoute cette valeur à l'objet req qui est transmis au route
         if(req.body.userId && req.body.userId !== userId){//si on a un user id dans le corps de la requete qui est différent de l'user id
          throw 'User ID non valable !';  
         } else {
